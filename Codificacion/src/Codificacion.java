9f91146a27d09fe01d32e895c70f1c0b11be004b
Jesús Pérez <jjchiva94@hotmail.com>
2019-10-29 17:15:27 +0100

    Proyecto finalizado

--- Codificacion/src/Codificacion.java
+++ Codificacion/src/Codificacion.java
@@ -19,39 +19,41 @@ public class Codificacion {
         return numero;
     };
 
-    public static ArrayList<Character> Descifrado(ArrayList<Character> numero){
+    public static String Descifrado(ArrayList<Character> numero){
 
         ArrayList<Character> frase = new ArrayList<Character>();
+        String listString = "";
+
 
         for (int i = 0; i < numero.size(); i++) {
 
             char descifrado = (char) (numero.get(i) - 21 - i);
             frase.add(descifrado);
-
-            System.out.print(descifrado);
+            listString = listString + descifrado;
 
         }
 
-        return frase;
+
+        return listString;
     };
 
 
 
-    public static void main(String[] args) {
-
-        ArrayList<Character> frase_cifrado = new ArrayList<Character>();
-        ArrayList<Character> frase_descifrado = new ArrayList<Character>();
-
-        Scanner sc = new Scanner(System.in);
-
-        String texto = sc.nextLine();
-
-        frase_cifrado = Cifrado(texto);
-
-        System.out.println(frase_cifrado);
-
-        frase_descifrado = Descifrado(frase_cifrado);
-
-    }
+//    public static void main(String[] args) {
+//
+//        ArrayList<Character> frase_cifrado = new ArrayList<Character>();
+//        ArrayList<Character> frase_descifrado = new ArrayList<Character>();
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        String texto = sc.nextLine();
+//
+//        frase_cifrado = Cifrado(texto);
+//
+//        System.out.println(frase_cifrado);
+//
+//        frase_descifrado = Descifrado(frase_cifrado);
+//
+//    }
 }
 
--- Codificacion/src/Main.java
+++ Codificacion/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Character> frase_cifrado = new ArrayList<Character>();
+        String frase_descifrado;
+
         System.out.println("\n***** Codificador César *****");
         // Creo objetos Scanner y CodificadorCesar
         Scanner tec = new Scanner(System.in).useLocale(Locale.US);
@@ -15,10 +20,16 @@ public class Main {
         System.out.print("Introduce el mensaje a codificar\n> ");
         String mensaje = tec.nextLine();
         System.out.println("");
+
+        //Combina 2 codificadores
         String criptograma = codificador.codificar(mensaje);
-        System.out.println("Mensaje codificado:\n" + criptograma);
+        frase_cifrado = Codificacion.Cifrado(criptograma);
+        frase_descifrado = Codificacion.Descifrado(frase_cifrado);
+
+
+        System.out.println("Mensaje codificado:\n" + frase_descifrado);
         System.out.println("");
-        String mensajeDecodificado = codificador.decodificar(criptograma);
+        String mensajeDecodificado = codificador.decodificar(frase_descifrado);
         System.out.println("Mensaje decodificado:\n" + mensajeDecodificado);
         System.out.print("*****************************");
     }
